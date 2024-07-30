f090e13c977ba8a368585e8fea49b9f92b7f5459
DESKTOP-RIENOSP\user1 <Sum-yujin>
2024-07-30 17:32:57 +0900

    240729 ~30 / 컬렉션, 예외 등

---
+++ 09_collection/src/run/ListRun.java
@@ -0,0 +1,14 @@
+package run;
+
+import service.ListService;
+
+public class ListRun {
+	public static void main(String[] args) {
+		
+		ListService service = new ListService();
+		
+//		service.test1();
+//		service.test2();
+		service.test3();
+	}
+}
