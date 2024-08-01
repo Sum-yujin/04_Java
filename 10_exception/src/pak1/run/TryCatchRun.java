b2a98de85f5121121584d4fb55a7c2d5e9eca6c0
DESKTOP-RIENOSP\user1 <Sum-yujin>
2024-08-01 15:43:39 +0900

    240801_interface , 전체적 배운 코드 이용해서 만들기 등

--- 10_exception/src/pak1/run/TryCatchRun.java
+++ 10_exception/src/pak1/run/TryCatchRun.java
@@ -8,9 +8,9 @@ public class TryCatchRun {
 		TryCatchService service = new TryCatchService();
 	
 //		service.test1();
-//		service.test2();
+		service.test2();
 //		service.test3();
 //		service.test4();
-		service.test5();
+//		service.test5();
 	}
 }
---
+++ 10_exception/src/pak4/run/CharRun.java
@@ -0,0 +1,14 @@
+package pak4.run;
+
+import pak4.service.CharService;
+
+public class CharRun {
+	public static void main(String[] args) {
+		CharService service = new CharService();
+
+//		service.fileOutput1();
+//		service.fileOutput2();
+//		service.fileInput1();
+		service.fileInput2();
+	}
+}
